4bf913584c9c2dedb129ea97cddfb40818c8cfa5
llOldmenll <nosovmak@gmail.com>
2018-05-05 23:21:06 +0300

    menus, prefabs and spawner were added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject mainMenu;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PauseGame(){
+        mainMenu.SetActive(true);
+    }
+
+    public void ResumeGame(){
+        mainMenu.SetActive(false);
+    }
+
+    public void RestartGame(){
+        mainMenu.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] tetromino;
+
+	void Start () {
+        CreateRandomTetramino();
+	}
+	
+    void CreateRandomTetramino(){
+        Instantiate(tetromino[Random.Range(0, tetromino.Length)], 
+                    transform.position, Quaternion.identity);
+    }
+}
